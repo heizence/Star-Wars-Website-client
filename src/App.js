fbc30e4e252de209680cc9fdcd41e7db64b3b8ec
Doheon Keum <heizence6626@gmail.com>
2020-03-11 17:53:43 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route, Link, Switch} from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import './App.css';
 import MainPage from './components/MainPage'
 import CategoryPage from './components/CategoryPage'
-import InformationPage from './components/InformationPage'
+import EachDataPage from './components/EachDataPage'
 
 function App() {
   return (
     <div>
       <Route path="/" exact component={MainPage}/>
       <Route path="/:category" exact render={({ match }) => <CategoryPage category={match.params.category}/>} />
-      <Route path="/:category/info" exact component={InformationPage} />
+      <Route path="/:category/:info" exact render={({ match }) => <EachDataPage match={match.params}/>} />
     </div>
   );
 }
