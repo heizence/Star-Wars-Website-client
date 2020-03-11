0aca2eab828267b72e4b3c4e68dc95c328c4488c
Doheon Keum <heizence6626@gmail.com>
2020-03-11 12:23:11 +0900

    modified to handle all kind of data, search function is added

--- src/reduxFiles/reducers.js
+++ src/reduxFiles/reducers.js
@@ -1,31 +1,45 @@
-import { REQUEST_PEOPLE_PENDING, REQUEST_PEOPLE_SUCCESS, REQUEST_PEOPLE_FAILED } from './actions'
-// import { combineRecucers } from 'redux'
+import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, SEARCH } from './actions'
 
-const peopleInitialState = {
+const dataInitialState = {
     isPending: false,
-    people: [],
+    data: [],
     error: ''
 }
 
-export const getPeople = (state=peopleInitialState, action={}) => {
+const searchInitialState = {
+    text: ''
+}
+
+export const fetchData = (state=dataInitialState, action={}) => {
     switch(action.type) {
-        case REQUEST_PEOPLE_PENDING:
+        case REQUEST_PENDING:
             return {
                 ...state,
                 isPending: true
             }
-        case REQUEST_PEOPLE_SUCCESS:
+        case REQUEST_SUCCESS:
             return {
                 ...state,
-                people: action.payload
+                data: action.payload
             }
-        case REQUEST_PEOPLE_FAILED:
+        case REQUEST_FAILED:
             return {
                 ...state,
                 error: action.payload
             }
 
         default:
-            return state;
+            return state
     }
 }
+
+export const searchData = (state=searchInitialState, action={}) => {
+    switch(action.type) {
+        case SEARCH:
+            return {
+                text: action.payload
+            }
+        default:
+            return state
+    }
+} 
