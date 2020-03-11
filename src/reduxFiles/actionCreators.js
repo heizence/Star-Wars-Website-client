7397e50ee0cef688867e949820cb362cf5f829af
Doheon Keum <heizence6626@gmail.com>
2020-03-11 12:21:32 +0900

    middleware modified, other category actions are added

--- src/reduxFiles/actionCreators.js
+++ src/reduxFiles/actionCreators.js
@@ -1,13 +1,18 @@
 import axios from 'axios'
-import { REQUEST_PEOPLE_PENDING, REQUEST_PEOPLE_SUCCESS, REQUEST_PEOPLE_FAILED } from './actions'
+import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, SEARCH } from './actions'
 
-export const requestPeople = (dispatch) => {
-    dispatch({ type: REQUEST_PEOPLE_PENDING })
-    axios.get('https://swapi.co/api/people/?format=json')
+export const requestData = (category) => (dispatch) => {
+    console.log('category is : ', category)
+    dispatch({ type: REQUEST_PENDING })
+    axios.get(`https://swapi.co/api/${category}/?format=json`)
     .then(res => {
-        dispatch({ type: REQUEST_PEOPLE_SUCCESS , payload: res.data.results })
+        dispatch({ type: REQUEST_SUCCESS , payload: res.data.results })
     })
     .catch(error => {
-        dispatch({ type: REQUEST_PEOPLE_FAILED, payload: error })
+        dispatch({ type: REQUEST_FAILED, payload: error })
     })
+}
+
+export const requestSearch = (dispatch, value) => {
+    dispatch({ type: SEARCH, payload: value})
 }
