89e865690266cb3ba3e310a06647d97b875e4f02
Doheon Keum <heizence6626@gmail.com>
2020-03-14 18:15:42 +0900

    backend initial setting

---
+++ server/app.js
@@ -0,0 +1,9 @@
+//const https = require('https')
+const express = require('express')
+const app = express()
+const port = 3001;
+
+// test
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
