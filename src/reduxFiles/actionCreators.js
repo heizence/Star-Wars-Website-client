2f2edb44187ead8df801ce54f88225280fad443e
Doheon Keum <heizence6626@gmail.com>
2020-03-16 23:52:41 +0900

    fetchUrl function deleted, modified

--- src/reduxFiles/actionCreators.js
+++ src/reduxFiles/actionCreators.js
@@ -1,40 +1,18 @@
-import axios from 'axios'
-import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, 
-    REQUEST_URL_PENDING, REQUEST_URL_SUCCESS, REQUEST_URL_FAILED, SEARCH } from './actions'
+import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, SEARCH
+} from './actions'
 
-
-export const requestData = (category) => (dispatch) => {
-    console.log('category is : ', category)
+export const requestData = (category, index) => (dispatch) => {
     dispatch({ type: REQUEST_PENDING })
-    axios.get(`https://swapi.co/api/${category}/?format=json`)
-    .then(res => {
-        dispatch({ type: REQUEST_SUCCESS, payload: res.data.results, category })
+    fetch(`https://swapi.co/api/${category}/?page=${index}&format=json`)
+    .then(res => res.json())
+    .then(data => {
+        dispatch({ type: REQUEST_SUCCESS, payload: data.results, category })
     })
     .catch(error => {
         dispatch({ type: REQUEST_FAILED, payload: error })
     })    
 }
 
-const requestURL = (category) => (dispatch) => {
-    console.log('category is : ', category)
-    dispatch({ type: REQUEST_URL_PENDING })
-    axios.get(`https://swapi.co/api/${category}/?format=json`)
-    .then(res => {
-        dispatch({ type: REQUEST_URL_SUCCESS, payload: res.data.results, category })
-    })
-    .catch(error => {
-        dispatch({ type: REQUEST_URL_FAILED, payload: error })
-    })    
-}
-
 export const requestSearch = (dispatch, value) => {
     dispatch({ type: SEARCH, payload: value})
-}
-
-export const requestAllURL = (dispatch) => {
-    let category = [ 'people', 'vehicles', 'planets', 'starships', 'species', 'films' ]
-    
-    for (let i=0; i<category.length; i++) {
-        dispatch(requestURL(category[i]))
-    }
 }
--- src/reduxFiles/reducers.js
+++ src/reduxFiles/reducers.js
@@ -1,5 +1,5 @@
-import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, 
-    REQUEST_URL_PENDING, REQUEST_URL_SUCCESS, REQUEST_URL_FAILED, SEARCH } from './actions'
+import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, SEARCH
+} from './actions'
 
 const dataInitialState = {
     isPending: false,
@@ -10,19 +10,6 @@ const dataInitialState = {
 
 const searchInitialState = {
     text: ''
-}
-
-const allUrlInitialState = {
-    isPending: false,
-    url: {
-        people: [],
-        vehicles: [],
-        planets: [],
-        starships: [],
-        species: [],
-        films: []
-    },
-    error: ''
 }
 
 export const fetchData = (state=dataInitialState, action={}) => {
@@ -43,40 +30,6 @@ export const fetchData = (state=dataInitialState, action={}) => {
                 ...state,
                 error: action.payload
             }
-        default:
-            return state
-    }
-}
-
-export const fetchAllURL = (state=allUrlInitialState, action={}) => {
-    switch(action.type) {
-        case REQUEST_URL_PENDING:
-            return {
-                ...state,
-                isPending: true
-            }
-        case REQUEST_URL_SUCCESS:       
-            //console.log('reducer에서 확인 : ', action.category, action.payload)  
-            
-            let nameAndUrl = {}
-            for (let i=0; i<action.payload.length; i++) {
-                nameAndUrl[action.payload[i].url] = action.payload[i].name || action.payload[i].title
-            }   
-            
-            return {
-                ...state,
-                isPending: false,
-                url: {
-                    ...state.url,
-                    [action.category] : nameAndUrl
-                }
-            }
-        case REQUEST_URL_FAILED:
-            return {
-                ...state,
-                error: action.payload
-            }
-
         default:
             return state
     }
