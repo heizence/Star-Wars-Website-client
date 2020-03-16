d95445bb46b17a5054816e9cc986a94e3aa9e096
Doheon Keum <heizence6626@gmail.com>
2020-03-16 23:51:37 +0900

    route added

--- server/routes/getData.js
+++ server/routes/getData.js
@@ -1,2 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const fetch = require('node-fetch')
+
+router.route('/get/eachdata').get((req, res) => {
+    console.log('요청 확인 : ', req.query)
+
+    let { category, index } = req.query
+
+    fetch(`https://swapi.co/api/${category}/${index}/?format=json`)
+    .then(response => response.json())
+    .then(json => {
+        console.log('응답 확인 : ', json, '\n')
+        res.status(200).send(json)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(404).send(err)
+    })
+})
+
+module.exports = router
