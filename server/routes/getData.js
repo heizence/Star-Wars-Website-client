c49d4e99d385fd31add251c78915eb6450cc72e9
Doheon Keum <heizence6626@gmail.com>
2020-03-17 21:17:45 +0900

    swap API applied

--- server/routes/getData.js
+++ server/routes/getData.js
@@ -1,22 +1,27 @@
 const express = require('express')
 const router = express.Router()
-const fetch = require('node-fetch')
+
+// star wars API
+const swapi = require('swapi-node')
 
 router.route('/get/eachdata').get((req, res) => {
     console.log('요청 확인 : ', req.query)
 
-    let { category, index } = req.query
+    let { category, name } = req.query
 
-    fetch(`https://swapi.co/api/${category}/${index}/?format=json`)
-    .then(response => response.json())
-    .then(json => {
-        console.log('응답 확인 : ', json, '\n')
-        res.status(200).send(json)
+    swapi.get(`https://swapi.co/api/${category}/?format=json`)
+    .then((result) => {
+        //console.log('요청 결과 확인 : ', result.results);
+        let dataToSend = result.results.filter(element => {
+            return element.name || element.title === name
+        })
+        console.log('걸러진 데이터 : ', dataToSend)
+        res.status(200).send(dataToSend)
     })
     .catch(err => {
         console.log(err)
         res.status(404).send(err)
-    })
+    })    
 })
 
 module.exports = router
