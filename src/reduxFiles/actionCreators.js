a0203f3cf819f7d77dfca417fd4800a8503ef361
Doheon Keum <heizence6626@gmail.com>
2020-04-12 23:54:10 +0900

    request method modified

--- src/reduxFiles/actionCreators.js
+++ src/reduxFiles/actionCreators.js
@@ -1,13 +1,21 @@
 import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, SEARCH
 } from './actions'
+import { serverAddress } from '../serverAddress'
 
-export const requestData = (category, index) => (dispatch) => {
-    console.log('actionCreater : ', `https://swapi.co/api/${category}/?page=${index}&format=json`)
+export const requestData = (category, method, name) => (dispatch) => {
+    console.log('category : ' , category, 'method : ', method, 'name : ', name)
+    console.log('actionCreater : ', `${serverAddress}/${method}?category=${category}`)
     dispatch({ type: REQUEST_PENDING })
-    fetch(`https://swapi.co/api/${category}/?page=${index}&format=json`)
+
+    // if name exists, getspecificdata request, or getnames request
+    let requestAddress = name ? `${serverAddress}/${method}?category=${category}&name=${name}`
+    : `${serverAddress}/${method}?category=${category}` 
+    
+    fetch(requestAddress)
     .then(res => res.json())
     .then(data => {
-        dispatch({ type: REQUEST_SUCCESS, payload: data.results, category })
+        console.log('actioncreators result : ', data)
+        dispatch({ type: REQUEST_SUCCESS, payload: data, category })
     })
     .catch(error => {
         dispatch({ type: REQUEST_FAILED, payload: error })
