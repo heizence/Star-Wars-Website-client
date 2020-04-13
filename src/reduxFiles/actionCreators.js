563e5f54b4e1278300635679c844fd16f66ba40d
Doheon Keum <heizence6626@gmail.com>
2020-04-13 15:52:45 +0900

    reduxfiles modified

--- src/reduxFiles/actionCreators.js
+++ src/reduxFiles/actionCreators.js
@@ -3,18 +3,18 @@ import { REQUEST_PENDING, REQUEST_SUCCESS, REQUEST_FAILED, SEARCH
 import { serverAddress } from '../serverAddress'
 
 export const requestData = (category, method, name) => (dispatch) => {
-    console.log('category : ' , category, 'method : ', method, 'name : ', name)
-    console.log('actionCreater : ', `${serverAddress}/${method}?category=${category}`)
+    //console.log('category : ' , category, 'method : ', method, 'name : ', name)
+    //console.log('actionCreater : ', `${serverAddress}/${method}?category=${category}`)
     dispatch({ type: REQUEST_PENDING })
 
-    // if name exists, getspecificdata request, or getnames request
+    // if name exists, getdata request, or getnames request
     let requestAddress = name ? `${serverAddress}/${method}?category=${category}&name=${name}`
     : `${serverAddress}/${method}?category=${category}` 
     
     fetch(requestAddress)
     .then(res => res.json())
     .then(data => {
-        console.log('actioncreators result : ', data)
+        //console.log('actioncreators result : ', data)
         dispatch({ type: REQUEST_SUCCESS, payload: data, category })
     })
     .catch(error => {
