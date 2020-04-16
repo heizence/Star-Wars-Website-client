e47337c0a738b668a0d06e9f0762262830dc3f92
Doheon Keum <heizence6626@gmail.com>
2020-04-16 19:45:39 +0900

    App.js modified

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import MainPage from './components/MainPage'
 import CategoryPage from './components/CategoryPage'
 import EachDataPage from './components/EachDataPage'
+import Signin from './components/Signin';
 
 function App() {
   return (
     <div>
       <Route path="/" exact component={MainPage}/>
-      <Route path="/:category/" exact render={({ match, location }) => <CategoryPage category={match.params.category} location={location}/>} />
-      <Route path="/:category/:name" exact render={({ match, location }) => <EachDataPage match={match.params} location={location}/>} />
+      <Route path="/signin" exact component={Signin} />
+      <Route path="/category/:category" exact render={({ match, location }) => <CategoryPage category={match.params.category} location={location}/>} />
+      <Route path="category/:category/:name" exact render={({ match, location }) => <EachDataPage match={match.params} location={location}/>} />      
     </div>
   );
 }
