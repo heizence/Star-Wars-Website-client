e57be395a1506cc8d0b68b2e3893e72b28419f5f
Doheon Keum <heizence6626@gmail.com>
2020-04-17 19:11:14 +0900

    Navbar modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { requestSignout } from '../reduxFiles/actionCreators'
+import { serverAddress } from '../serverAddress'
+
+const mapStateToProps = (state) => {
+  return {
+      isLoggedIn: state.handleUser.isLoggedIn,
+      username: state.handleUser.username
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onRequestSignout: () => requestSignout(dispatch)
+  }
+}
+
+const fetchSignout = (props) => {
+  fetch(`${serverAddress}/user/signout`, { cors: 'no-cors'})
+  .then(res => {
+    console.log('res : ' , res)
+    window.alert('Logged out!')
+    props.onRequestSignout()
+  })
+  .catch(error => console.log(error))
+}
 
 const NavigationBar = (props) => {
-
   return (
     <div id="navbar">
-      <a href='/'>
+    <Link to={'/'}>
       <img id="main-logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/3/36/SW_opening_crawl_logo.svg/1024px-SW_opening_crawl_logo.svg.png" 
       width="200px" heigh="40" alt="" ></img>      
-      </a>
+      </Link>
         <div id='navbar-text'>        
         <ul>
-          <li><a href='/signin'>Sign in</a></li>
-          <li><a href='/'>Home</a></li>
-          <li><a href='/board'>Board</a></li>         
+          {props.isLoggedIn ?
+            <li style={{color: 'yellow'}}>Mypage</li> : ''
+          }
+          {props.isLoggedIn ?           
+          <li onClick={() =>fetchSignout(props)}>Sign out</li> :
+          <li><Link to={'/signin'}>Sign in</Link></li>
+          }          
+          <li><Link to={'/'}>Home</Link></li>
+          <li><Link to={'/board'}>Board</Link></li>         
         </ul>
       </div>
     </div>
   );
 }
 
-export default NavigationBar;
+export default connect(mapStateToProps, mapDispatchToProps)(NavigationBar);
