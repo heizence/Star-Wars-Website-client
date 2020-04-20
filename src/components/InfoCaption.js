019b4b4ed30c7505219124e4a62eec50c5ee71ca
Doheon Keum <heizence6626@gmail.com>
2020-04-20 18:44:09 +0900

    InfoCaption modified

--- src/components/InfoCaption.js
+++ src/components/InfoCaption.js
@@ -5,7 +5,8 @@ const InfoCaption = () => {
     return (
         <div className="APIinfo">
             Informations are provided by Star Wars API by davimacedo. If you would like to know about it, 
-            visit <a href={url} target="blank" style={{textDecoration: 'none', color: 'blue'}}>{url}</a>
+            visit here : 
+            <a href={url} target="blank" style={{textDecoration: 'none', color: 'blue'}}> Back4app SWAPI</a>
         </div>
     )
 }
