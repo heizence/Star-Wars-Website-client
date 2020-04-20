1ac9196918d417775a1314473052774b6a0d81f8
Doheon Keum <heizence6626@gmail.com>
2020-04-20 18:43:05 +0900

    Withdrawal page added

--- src/components/Mypage.js
+++ src/components/Mypage.js
@@ -10,7 +10,8 @@ import { serverAddress } from '../serverAddress'
 const mapStateToProps = (state) => {
     return {
         isLoggedIn: state.handleUser.isLoggedIn,
-        userObj: state.handleUser.userObj
+        user: state.handleUser.user,
+        token: state.handleUser.token
     }
 }
   
@@ -31,12 +32,30 @@ class Mypage extends Component {
     }
   }
 
-  changeUsername = () => {
+  changeUsername = async () => {
     if (this.state.newUsername === '') {
         window.alert('Enter a new username.')
     }
     else {
-        this.fetchModify(this.state.newUsername, null)
+      try {
+        let res = await  fetch(`${serverAddress}/user/checkuserinfo`, 
+        { 
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({ category: 'username', subject: this.state.newUsername })      
+        })
+
+        if (res.status === 201) {
+          window.alert(`username already exists.`)         
+        }
+        else if (res.status === 200) {
+            this.fetchModify(this.state.newUsername, null)
+        }
+      }
+      catch(error) {
+        window.alert('Error occured! Please try again later.')
+        console.log(error)
+      }
     }
   }
 
@@ -53,7 +72,7 @@ class Mypage extends Component {
   }
 
   fetchModify = (newUsername, newPassword) => {
-    let reqBody = { newUsername, newPassword }
+    let reqBody = { newUsername, newPassword, token: this.props.token }
 
     fetch(`${serverAddress}/user/updateuser`, 
     { 
@@ -63,34 +82,41 @@ class Mypage extends Component {
     })
     .then(res => {
         if (res.status === 200) {
-            window.alert(`User information has been modified.`)
+          newUsername ? window.alert(`Username has been modified.`)
+          : window.alert(`Password has been modified.`)
         }
     })
-    .catch(error => console.log(error))
-  }
-
-  fetchDeleteAccount = () => {
-    let password = window.prompt(`You are going to delete your account. If you want to proceed, enter your password and press OK.`)
-
-    if (password === '') {
-        window.alert(`Enter your password`)
-        return
-    }
-    fetch(`${serverAddress}/user/deleteuser?password=${password}`)
-    .then(res => {
-        if (res.status === 200) {
-            window.alert(`User account has been deleted.`)  
-            this.props.onRequestSignout()     
-        }
-        else if (res.status === 201) {
-            window.alert(`Password incorrect!`)
-        }
+    .catch(error => {
+      window.alert('Error occured! Please try again later.')
+      console.log(error)
     })
-    .catch(error => console.log(error))
   }
+
+  // fetchDeleteAccount = () => {
+  //   let password = window.prompt(`You are going to delete your account. If you want to proceed, enter your password and press OK.`)
+
+  //   if (password === '') {
+  //       window.alert(`Enter your password`)
+  //       return
+  //   }
+  //   fetch(`${serverAddress}/user/deleteuser?password=${password}&token=${this.props.token}`, {
+  //     method: 'DELETE',
+  //     headers: {'Content-Type': 'application/json'}
+  //   })
+  //   .then(res => {
+  //       if (res.status === 200) {
+  //           window.alert(`User account has been deleted.`)  
+  //           this.props.onRequestSignout()     
+  //       }
+  //       else if (res.status === 201) {
+  //           window.alert(`Password incorrect!`)
+  //       }
+  //   })
+  //   .catch(error => console.log(error))
+  // }
 
   render() {
-    if (this.props.isLoggedIn) {
+    if (!this.props.isLoggedIn) {
       return <Redirect to='/' />
     }
     else {
@@ -100,8 +126,8 @@ class Mypage extends Component {
           <div style={{paddingTop: '50px', minHeight: '70vh'}}>
             <h1 className="titleCaption">MY PAGE</h1>
             <div style={{textAlign: "left", width: '40%', maxWidth: '500px', margin: 'auto'}}>
-                <div className="mypage-text">Email : test@test.com</div>
-                <div className="mypage-text">Username : username!</div>
+                <div className="mypage-text">Email : {this.props.user.email}</div>
+                <div className="mypage-text">Username : {this.props.user.username}</div>
                 <div style={{marginTop: '50px'}}>
                     <div className="mypage-text" style={{fontSize: '17px', color: 'yellow'}}>
                     <span>* You don't have to fill all blanks.</span>
@@ -110,10 +136,10 @@ class Mypage extends Component {
 
                     <div style={{color: 'white', fontSize: '20px'}}>Change Username</div>
                     <Input className="signin-form" type="text" name="username" 
-                    id="exampleEmail" placeholder="new username" autocomplete="off"                  
+                    id="exampleEmail" placeholder="new username" autoComplete="off"                  
                     onChange={(e) => this.setState({ newUsername: e.target.value })} />
                     <div>
-                        <Button id="signin-button" 
+                        <Button className="userform-button" 
                         style={{marginLeft: '10px', width: '130px', height: '60px', marginBottom: '20px'}}
                         onClick={() => this.changeUsername()}>Change Username
                         </Button>
@@ -123,7 +149,7 @@ class Mypage extends Component {
                 <div style={{marginTop: '20px'}}>
                     <div style={{color: 'white', fontSize: '20px'}}>Change Password</div>
                     <Input className="signin-form" type="password" name="password" 
-                    id="exampleEmail" placeholder="new password" autocomplete="off"                  
+                    placeholder="new password" autoComplete="off"                  
                     onChange={(e) => this.setState({ newPassword: e.target.value })} />
                     
                 </div>
@@ -131,7 +157,7 @@ class Mypage extends Component {
                 <div style={{marginTop: '20px'}}>
                     <div style={{color: 'white', fontSize: '20px'}}>Confirm Password</div>
                     <Input className="signin-form" type="password" name="password" 
-                    id="examplePassword" placeholder="password" autocomplete="off"
+                    placeholder="password" autoComplete="off"
                     onChange={(e) => this.setState({ newPasswordCheck: e.target.value})} />
                 </div>
 
@@ -148,7 +174,7 @@ class Mypage extends Component {
                 </div>
 
                 <div>
-                    <Button id="signin-button" 
+                    <Button className="userform-button" 
                     style={{marginLeft: '10px', width: '130px', height: '60px'}}
                     onClick={() => this.changePassword()}>Change Password
                     </Button>
@@ -161,8 +187,10 @@ class Mypage extends Component {
                 <Button id="goback-button">Go back to main page</Button>
                 </Link>
 
+                <Link to={'/withdraw'}>
                 <Button id="withdrawal-button" 
                 onClick={this.fetchDeleteAccount}>Membership Withdrawal</Button>
+                </Link>
             </div>
             
             </div>
