c6ee87673434dd492f15b70838e1321649d179fa
Doheon Keum <heizence6626@gmail.com>
2020-04-23 17:44:12 +0900

    RenderImage.js modified

--- src/components/RenderImage.js
+++ src/components/RenderImage.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
-const RenderImage = ({ image }) => {
+const RenderImage = ({ imagefile }) => {
+    console.log('Render Image : ', imagefile)
+    let imgStyle, tag
+
+    if (imagefile) {
+        imgStyle = { backgroundImage: `url(${imagefile.url})` }
+        tag = ''
+    }
+    else {
+        tag = <div style={{ marginTop: '50%', fontSize: '30px', fontWeight: 'bold' }}>NO<br></br>IMAGE</div>
+    }
     return (
-        <div className="contents-imgbox" style={{ backgroundImage: `url(${image})`}}>img</div>
+        <div className="contents-imgbox" style={imgStyle}>{tag}</div>
     )
 }
 
