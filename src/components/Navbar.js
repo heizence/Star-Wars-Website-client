edb0f0a14fd45848bc79c9a4455e4018ca8a3bdf
Doheon Keum <heizence6626@gmail.com>
2020-04-25 21:25:39 +0900

    Navbar.js modified

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 import { mapStateToProps, mapDispatchToProps } from '../reduxFiles/props'
 import { serverAddress } from '../serverAddress'
 
+// Signout request
 const fetchSignout = (props) => {
   console.log('fetchsignout executed')
   fetch(`${serverAddress}/user/signout?token=${sessionStorage.getItem('token')}`)
@@ -18,23 +19,36 @@ const fetchSignout = (props) => {
 }
 
 const NavigationBar = (props) => {
+  // Reset SearchField text to prevent filter error when redirecting
+  const resetSearchText = () => {
+    props.onSearchData('')
+  }
+  
+  console.log('sessionToken : ', sessionStorage)
+  
   return (
     <div id="navbar">
     <Link to={'/'}>
       <img id="main-logo" src="https://upload.wikimedia.org/wikipedia/en/thumb/3/36/SW_opening_crawl_logo.svg/1024px-SW_opening_crawl_logo.svg.png" 
-      width="200px" heigh="40" alt="" ></img>      
+      width="200px" heigh="40" alt="" 
+      onClick={resetSearchText}></img>      
       </Link>
         <div id='navbar-text'>        
         <ul>
-          {sessionStorage.length > 0 ? // Login status
-            <li style={{color: 'yellow'}}><Link to={'/mypage'}>My page</Link></li> : ''
+          {sessionStorage.token ? // if logged in
+            <li style={{color: 'yellow'}}
+            onClick={resetSearchText}
+            ><Link to={'/mypage'}>My page</Link></li> : ''
           }
-          {sessionStorage.length > 0 ?  // Login status  
-          <li onClick={() => fetchSignout(props)}>Sign out</li> :
-          <li><Link to={'/signin'}>Sign in</Link></li>
+          {sessionStorage.token ?  // if logged in
+          <li onClick={() => {
+            fetchSignout(props)
+            resetSearchText()
+          }}>Sign out</li> :
+          <li onClick={resetSearchText}><Link to={'/signin'}>Sign in</Link></li>
           }          
-          <li><Link to={'/'}>Home</Link></li>
-          <li><Link to={'/board'}>Board</Link></li>         
+          <li onClick={resetSearchText}><Link to={'/'}>Home</Link></li>
+          <li onClick={resetSearchText}><Link to={'/board'}>Board</Link></li>         
         </ul>
       </div>
     </div>
